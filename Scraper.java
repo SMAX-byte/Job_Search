9a66b2e645357f209853ccf39c0c002a13675fac
Samuel Maxey <smaxey@xula.edu>
2025-10-16 16:35:24 -0500

    Onee piece welcome meaasge in ascii art has been. customized

--- Scraper.java
+++ Scraper.java
@@ -2,18 +2,26 @@ package Job_Search;
 
 class Scraper {
     public static void main(String[] args) {
-        System.out.println("\n\nHello, World from Scraper.java!\n\n");
-        String[] lines = {
-            " OOO    N   N  EEEEE  PPPP   I  EEEEE  CCCC  EEEEE",
-            "O   O   NN  N  E      P   P  I  E     C      E",
-            "O   O   N N N  EEEE   PPPP   I  EEEE  C      EEEE",
-            "O   O   N  NN  E      P      I  E     C      E",
-            " OOO    N   N  EEEEE  P      I  EEEEE  CCCC  EEEEE"
-        };
+          // ANSI color codes
+        final String BLUE = "\u001B[34m";
+        final String WHITE = "\u001B[37m";
+        final String RESET = "\u001B[0m";
 
-        for (String line : lines) {
-            System.out.println(line);
+        // Banner with colors
+        String banner = BLUE + """
+                 ___   _  _  _____     ___  ___  ___  ___  ___ 
+                | _ \\ | \\| ||_   _|   | _ \\| __|| __|| __|| _ \\
+                |  _/ | .` |  | |     |  _/| _| | _| | _| |   /
+                |_|   |_|\\_|  |_|     |_|  |___||_|  |___||_|_\\
+                """ + RESET + WHITE + """
+                      .-''''-.
+                     / _  _  \\
+                     |(@)(@)|
+                     \\  __  /
+                      '-..-'
+                      Pirate Mode Engaged ☠️
+                """ + RESET;
 
-        }
+        System.out.println(banner);
     }
 }
