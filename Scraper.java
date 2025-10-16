27228445df31638768d26feda5482a6ab82546c8
Samuel Maxey <smaxey@xula.edu>
2025-10-16 16:23:47 -0500

    Add sttring array containing my  welcome message

--- Scraper.java
+++ Scraper.java
@@ -3,5 +3,17 @@ package Job_Search;
 class Scraper {
     public static void main(String[] args) {
         System.out.println("\n\nHello, World from Scraper.java!\n\n");
+        String[] lines = {
+            " OOO    N   N  EEEEE  PPPP   I  EEEEE  CCCC  EEEEE",
+            "O   O   NN  N  E      P   P  I  E     C      E",
+            "O   O   N N N  EEEE   PPPP   I  EEEE  C      EEEE",
+            "O   O   N  NN  E      P      I  E     C      E",
+            " OOO    N   N  EEEEE  P      I  EEEEE  CCCC  EEEEE"
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+
+        }
     }
 }
